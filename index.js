3b899634a95abdaf13bd7ab1adc91813c4bbba15
Malsha <malshapramodi2001@gmail.com>
2025-03-05 11:55:58 +0530

    created product model and controller

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ import mongoose from "mongoose";
 import userRouter from "./routes/userRouter.js";
 import jwt from "jsonwebtoken";
 import dotenv from "dotenv";
+import productRouter from "./routes/productRouter.js";
 
 dotenv.config();
 
@@ -40,6 +41,7 @@ app.use(
 )
 
 app.use("/api/users", userRouter);
+app.use("/api/products", productRouter);
 
 app.listen(5000, () => {
     console.log("Server is running on port 5000");

ded3159ae0aaafb2926f433491a39a7fdb8485d1
Malsha <malshapramodi2001@gmail.com>
2025-03-06 14:32:28 +0530

    add another order function

--- controllers/orderController.js
+++ controllers/orderController.js
@@ -44,3 +44,12 @@ export async function createOrder(req, res) {
         res.status(500).json({ message: error.message })
     }
 }
+
+export async function getOrders(req, res) {
+    try {
+        const orders = await Order.find({ email: req.user.email })
+        res.json(orders)
+    } catch (error) {
+        res.status(500).json({ message: error.message })
+    }
+}

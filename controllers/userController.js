1a899655a556869a8540570f027e9f460cfb050f
Malsha <malshapramodi2001@gmail.com>
2025-03-13 20:26:45 +0530

    User validation issue resolved

--- controllers/userController.js
+++ controllers/userController.js
@@ -5,102 +5,107 @@ import dotenv from "dotenv";
 dotenv.config();
 
 export function createUser(req, res) {
+  const newUserData = req.body;
 
-    const newUserData = req.body;
+  newUserData.password = bcrypt.hashSync(newUserData.password, 10);
+  const user = new User(newUserData);
 
-    newUserData.password = bcrypt.hashSync(newUserData.password, 10);
-    const user = new User(newUserData);
-
-
-    if (newUserData.type === "admin") {
-        if (req.user == null) {
-            res.json({
-                message: "Please login as administrator to create admin accounts"
-            })
-            return
-        }
-
-        if (req.user.type != "admin") {
-            res.json({
-                message: "You are not authorized to create admin accounts"
-            })
-            return
-        }
-    }
-
-
-    user.save().then(
-        () => res.json({
-            Message: "User created"
-        })
-    ).catch(
-        () => res.json({
-            Message: "Error"
-        })
-    )
-}
-
-export function loginUser(req, res) {
-    User.find({ email: req.body.email }).then(
-        (users) => {
-            if (users.length === 0) {
-                res.json({ Message: "User not found" })
-            } else {
-                const user = users[0]
-                const isPasswordCorrect = bcrypt.compareSync(req.body.password, user.password)
-
-                if (isPasswordCorrect) {
-                    const token = jwt.sign({ user }, process.env.SECRET)
-                    console.log(token)
-
-                    res.json(
-                        {
-                            Message: "User logged in",
-                            token: token
-                        }
-                    )
-                } else {
-                    res.json(
-                        {
-                            Message: "User not logged in (wrong password)"
-                        }
-                    )
-                }
-            }
-        }
-    )
-}
-
-export function deleteUser(req, res) {
-    User.deleteOne({ email: req.body.email }).then(
-        () => res.json({ Message: "User deleted" })
-    ).catch(
-        () => res.json({ Message: "Error:User cannot be deleted." })
-    )
-}
-
-export function isAdmin(req) {
-    if (!req.user) {
-        return false
+  if (newUserData.type === "admin") {
+    if (req.user == null) {
+      res.json({
+        message: "Please login as administrator to create admin accounts",
+      });
+      return;
     }
 
     if (req.user.type != "admin") {
-        return false
+      res.json({
+        message: "You are not authorized to create admin accounts",
+      });
+      return;
     }
+  }
 
-    return true
+  user
+    .save()
+    .then(() =>
+      res.json({
+        Message: "User created",
+      })
+    )
+    .catch(() =>
+      res.json({
+        Message: "Error",
+      })
+    );
+}
+
+export function loginUser(req, res) {
+  User.find({ email: req.body.email }).then((users) => {
+    if (users.length === 0) {
+      res.json({ Message: "User not found" });
+    } else {
+      const user = users[0];
+      const isPasswordCorrect = bcrypt.compareSync(
+        req.body.password,
+        user.password
+      );
+
+      if (isPasswordCorrect) {
+        const token = jwt.sign(
+          {
+            email: user.email,
+            firstName: user.firstName,
+            lastName: user.lastName,
+            isBlocked: user.isBlocked,
+            type: user.type,
+            profilePicture: user.profilePicture,
+          },
+          process.env.SECRET
+        );
+        console.log(token);
+
+        res.json({
+          Message: "User logged in",
+          token: token,
+        });
+      } else {
+        res.json({
+          Message: "User not logged in (wrong password)",
+        });
+      }
+    }
+  });
+}
+
+export function deleteUser(req, res) {
+  User.deleteOne({ email: req.body.email })
+    .then(() => res.json({ Message: "User deleted" }))
+    .catch(() => res.json({ Message: "Error:User cannot be deleted." }));
+}
+
+export function isAdmin(req) {
+  if (!req.user) {
+    return false;
+  }
+
+  if (req.user.type != "admin") {
+    return false;
+  }
+
+  return true;
 }
 
 export function isCustomer(req) {
-    if (req.user == null) {
-        return false
-    }
+  if (req.user == null) {
+    return false;
+  }
 
-    if (req.user.type != "customer") {
-        return false
-    }
+  if (req.user.type != "customer") {
+    return false;
+  }
 
-    return true
+  return true;
 }
 //Customer Acc - john.doe@example.com hashed_password_here
 

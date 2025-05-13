786a2ee8d9c27985539ac9413f671df927391a32
Malsha <malshapramodi2001@gmail.com>
2025-05-13 23:07:40 +0530

    cors added

--- index.js
+++ index.js
@@ -6,45 +6,44 @@ import jwt from "jsonwebtoken";
 import dotenv from "dotenv";
 import productRouter from "./routes/productRouter.js";
 import orderRouter from "./routes/orderRouter.js";
+import cors from "cors";
 
 dotenv.config();
-
 
 const app = express();
 
 const mongodburl = process.env.MONGO_DB_URI;
 
-mongoose.connect(mongodburl, {})
+app.use(cors());
+
+mongoose.connect(mongodburl, {});
 
 const connection = mongoose.connection;
 
 connection.once("open", () => {
-    console.log("MongoDB database connection established");
-})
+  console.log("MongoDB database connection established");
+});
 
 app.use(bodyParser.json());
 
-app.use(
-    (req, res, next) => {
-        const token = req.header("Authorization")?.replace("Bearer ", "")
-        // console.log(token)
+app.use((req, res, next) => {
+  const token = req.header("Authorization")?.replace("Bearer ", "");
+  // console.log(token)
 
-        if (token != null) {
-            jwt.verify(token, process.env.SECRET, (error, decoded) => {
-                if (!error) {
-                    req.user = decoded
-                }
-            }
-            )
-        }
-        next()
-    }
-)
+  if (token != null) {
+    jwt.verify(token, process.env.SECRET, (error, decoded) => {
+      if (!error) {
+        req.user = decoded;
+      }
+    });
+  }
+  next();
+});
 
 app.use("/api/users", userRouter);
 app.use("/api/products", productRouter);
 app.use("/api/orders", orderRouter);
 
 app.listen(5000, () => {
-    console.log("Server is running on port 5000");
+  console.log("Server is running on port 5000");
 });

c6d5b666230b2daf24fe2f089dc144cc8ad6bc64
Malsha <malshapramodi2001@gmail.com>
2025-10-19 01:26:43 +0530

    Product update added

--- routes/productRouter.js
+++ routes/productRouter.js
@@ -2,12 +2,16 @@ import express from "express";
 import {
   createProduct,
   deleteProduct,
+  getProductById,
   getProducts,
+  updateProduct,
 } from "../controllers/productController.js";
 
 const productRouter = express.Router();
 
 productRouter.post("/", createProduct);
 productRouter.get("/", getProducts);
+productRouter.get("/:productId", getProductById);
 productRouter.delete("/:productId", deleteProduct);
+productRouter.put("/:productId", updateProduct);
 export default productRouter;

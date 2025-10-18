a9490569678ecb88f8d42ca6425398b7bd7fb63f
Malsha <malshapramodi2001@gmail.com>
2025-10-18 11:11:57 +0530

    Add products updated

--- controllers/productController.js
+++ controllers/productController.js
@@ -2,31 +2,55 @@ import Product from "../models/product.js";
 import { isAdmin } from "./userController.js";
 
 export function createProduct(req, res) {
-    if (!isAdmin(req)) {
-        res.json({
-            message: "Please login as administrator to add products"
-        })
-        return
-    }
+  if (!isAdmin(req)) {
+    res.json({
+      message: "Please login as administrator to add products",
+    });
+    return;
+  }
 
-    const newProductData = req.body;
-    const product = new Product(newProductData)
+  const newProductData = req.body;
+  const product = new Product(newProductData);
 
-    product.save().then(() => {
-        res.json({
-            message: "Product created"
-        })
-
-    }).catch((error) => {
-        res.json({
-            message: error
-        })
+  product
+    .save()
+    .then(() => {
+      res.json({
+        message: "Product created",
+      });
     })
-
+    .catch((error) => {
+      res.status(403).json({
+        message: error,
+      });
+    });
 }
 
 export function getProducts(req, res) {
-    Product.find().then((products) => {
-        res.json(products)
+  Product.find().then((products) => {
+    res.json(products);
+  });
+}
+
+export function deleteProduct(req, res) {
+  if (!isAdmin(req)) {
+    res.status(403).json({
+      message: "Please login as asministrator to delete products",
+    });
+    return;
+  }
+
+  const productId = req.params.productId;
+
+  Product.deleteOne({ productId: productId })
+    .then(() => {
+      res.json({
+        message: "Product Deleted",
+      });
     })
+    .catch((error) => {
+      res.status(403).json({
+        message: error,
+      });
+    });
 }
